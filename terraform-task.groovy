f284c2d78143ca2f0b8233b2427e817e52f292e6
tmoraru <tatianamoraru1998@gmail.com>
2021-01-14 11:39:25 -0600

    execute terraform comands through Jenkins

--- terraform-task.groovy
+++ terraform-task.groovy
@@ -2,8 +2,10 @@ node {
     stage("Pull Repo"){
         git branch: 'solution', changelog: false, poll: false, url: 'https://github.com/ikambarov/terraform-task.git'
     }
+
+    withCredentials([usernamePassword(credentialsId: 'jenkins-aws-access-key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
     
-        stage("Terrraform Init"){
+      stage("Terrraform Init"){
             sh '''
                 cd sandbox/
                 terraform-0.13 version
@@ -16,6 +18,9 @@ node {
                 terraform-0.13 apply -auto-approve
             '''
         }
+}
+    
+      
     }    
 
 
