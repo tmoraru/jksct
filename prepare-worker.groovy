0b33b452a54cd9ed75ceb5c4462698ae9e3e5916
tmoraru <tatianamoraru1998@gmail.com>
2021-01-14 16:43:41 -0600

    all packages for Packer

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -49,6 +49,13 @@ node {
                 sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} mv terraform /usr/bin/terraform-0.11'
             }
 
+            stage("Install Packer"){
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install -y yum-utils'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo'
+                // sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} mv /usr/sbin/packer /usr/sbin/packer_original'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install -y packer'
+            }
+
     }
 
             }
