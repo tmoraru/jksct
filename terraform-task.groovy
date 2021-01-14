45967d6ee650c78ba7ff3ba7fa0693e92c3e01a5
tmoraru <tatianamoraru1998@gmail.com>
2021-01-14 10:44:00 -0600

    execute terraform comands through Jenkins

--- terraform-task.groovy
+++ terraform-task.groovy
@@ -1,17 +1,22 @@
-node{
+node {
     stage("Pull Repo"){
         git branch: 'solution', changelog: false, poll: false, url: 'https://github.com/ikambarov/terraform-task.git'
     }
-    stage("Terraform Init"){
-        ssh ' terraform-0.13 version'
-        
-    }
-
-    stage("Pull Terraform Apply"){
-        ssh 'echo "Terraform Apply"'
-    }
     
-}
+        stage("Terrraform Init"){
+            sh '''
+                cd sandbox/
+                terraform-0.13 version
+                terraform-0.13 init
+            '''
+        }
+        stage("Terraform Apply"){
+            sh '''
+                cd sandbox/
+                terraform-0.13 apply -auto-approve
+            '''
+        }
+    }    
 
 
         
